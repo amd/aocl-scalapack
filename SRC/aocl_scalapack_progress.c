afd6dbcc930b0eb0b40e3e10a6de46b1732ca439
nprasadm <nprasadm@amd.com>
2022-04-26 11:14:41 +0530

    SCALAPACK: AOCL scalapack progress implementation in PDGETRF API
      -- AOCL libraries are used for lengthy computations which can go
         on for hours or days, once the operation is started, the user
         doesn’t get any update on current state of the computation.
         This (AOCL progress) feature enables user to receive a periodic
         update from the libraries.
      -- User can define AOCL_scalapack_Progress in application and register
         a callback with the library if it is interested in receiving
         the periodic update.
      -- The library invokes this callback periodically with information
         about current state of the operation.
      -- The update frequency is statically set in the code, it can be
         modified as needed if the library is built from source.
    
    Sample test application showing usage of AOCL Progress feature with
    PDGETRF API is included.
    AMD-Internal: [CPUPL-2113]
    Change-Id: I58daea17c1318ab710cd19437af7c647d04389a1

--- SRC/aocl_scalapack_progress.c
+++ SRC/aocl_scalapack_progress.c
@@ -9,59 +9,25 @@
 #include "aocl_scalapack_progress.h"
 
 aocl_scalapack_progress_callback aocl_scalapack_progress_ptr_ = NULL;
+#define aocl_scalapack_set_progress aocl_scalapack_set_progress_
 
+#ifdef __STDC__
 void aocl_scalapack_set_progress( aocl_scalapack_progress_callback func )
+#else
+    void aocl_scalapack_set_progress( func )
+       aocl_scalapack_progress_callback  func;
+#endif
 {
     aocl_scalapack_progress_ptr_ = func;
 }
 
-/**
-    Wrapper functions for 'aocl_scalapack_set_progress' function
-    to enable Fortran to C calls.
-**/
-void aocl_scalapack_set_progress_( aocl_scalapack_progress_callback func )
+void aocl_scalapack_progress_(char* api, integer *lenapi, integer* progress, 
+                                     integer* current_process, integer *total_processes)
 {
-    aocl_scalapack_set_progress(func);
-}
-
-void AOCL_SCALAPACK_SET_PROGRESS_( aocl_scalapack_progress_callback func )
-{
-    aocl_scalapack_set_progress(func);
-}
-
-void AOCL_SCALAPACK_SET_PROGRESS( aocl_scalapack_progress_callback func )
-{
-    aocl_scalapack_set_progress(func);
-}
-
-integer aocl_scalapack_progress_(const char* const api, const integer *lenapi, const integer* progress,
-                                     const integer* current_process, const integer *total_processes)
-{
-   integer ret = 0;
-   if (aocl_scalapack_progress_ptr_  != NULL ) {
+   integer ret;
+   if (aocl_scalapack_progress_ptr_  != NULL ) { 
       ret = aocl_scalapack_progress_ptr_ ( api, lenapi, progress, current_process, total_processes);
    }
-
-   return ret;
-}
-/**
-    Wrapper functions for 'aocl_scalapack_progress_' function
-    to enable Fortran to C calls.
-**/
-integer aocl_scalapack_progress(const char* const api, const integer* lenapi, const integer* progress,
-    const integer* current_process, const integer* total_processes)
-{
-    return aocl_scalapack_progress_(api, lenapi, progress, current_process, total_processes);
-}
-
-integer AOCL_SCALAPACK_PROGRESS(const char* const api, const integer* lenapi, const integer* progress,
-    const integer* current_process, const integer* total_processes)
-{
-    return aocl_scalapack_progress_(api, lenapi, progress, current_process, total_processes);
-}
-
-integer AOCL_SCALAPACK_PROGRESS_(const char* const api, const integer* lenapi, const integer* progress,
-    const integer* current_process, const integer* total_processes)
-{
-    return aocl_scalapack_progress_(api, lenapi, progress, current_process, total_processes);
+   
+   return;
 }
--- SRC/aocl_scalapack_progress.h
+++ SRC/aocl_scalapack_progress.h
@@ -9,9 +9,10 @@
 #define _AOCL_SCALAPACK_PROGRESS_
 
 #include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 /** Declarations **/
+
+#define aocl_scalapack_set_progress_ aocl_scalapack_set_progress
 
 #if defined(ENABLE_ILP64)
 typedef int64_t integer;
@@ -22,53 +23,19 @@ typedef unsigned long uinteger;
 #endif
 
 typedef integer ( *aocl_scalapack_progress_callback )(
-const char * const api,
-const integer  *lenapi,
-const integer  *progress,
-const integer  *current_process,
-const integer  *total_processes
+char *api,
+integer  *lenapi,
+integer  *progress,
+integer  *current_process,
+integer  *total_processes
 );
+
+
+integer aocl_scalapack_progress ( char* api, integer *lenapi, integer* progress, 
+                                     integer* current_process, integer *total_processes );
 
 aocl_scalapack_progress_callback aocl_scalapack_progress_ptr_;
 
-integer aocl_scalapack_progress_(
-    const char* const api,
-    const integer* lenapi,
-    const integer* progress,
-    const integer* current_process,
-    const integer* total_processes
-);
-/** Wrapper function declarations for
-    'aocl_scalapack_progress_' function
-**/
-integer aocl_scalapack_progress(
-    const char* const api,
-    const integer* lenapi,
-    const integer* progress,
-    const integer* current_process,
-    const integer* total_processes
-);
-integer AOCL_SCALAPACK_PROGRESS(
-    const char* const api,
-    const integer* lenapi,
-    const integer* progress,
-    const integer* current_process,
-    const integer* total_processes
-);
-integer AOCL_SCALAPACK_PROGRESS_(
-    const char* const api,
-    const integer* lenapi,
-    const integer* progress,
-    const integer* current_process,
-    const integer* total_processes
-);
-
 void aocl_scalapack_set_progress( aocl_scalapack_progress_callback func );
-/** Wrapper function declarations for
-    'aocl_scalapack_set_progress' function
-**/
-void aocl_scalapack_set_progress_( aocl_scalapack_progress_callback func );
-void AOCL_SCALAPACK_SET_PROGRESS( aocl_scalapack_progress_callback func );
-void AOCL_SCALAPACK_SET_PROGRESS_( aocl_scalapack_progress_callback func );
 
 #endif // _AOCL_SCALAPACK_PROGRESS_
