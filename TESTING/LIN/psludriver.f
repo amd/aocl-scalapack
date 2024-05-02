b3333743f3b22c72d5cbde6a5e9e97c22b8fbddf
Sumit Kothari <skothari@amd.com>
2024-05-02 08:46:45 +0530

    Implemented functionality to validate early return for the psgetrf API
    Signed-off-by: Sumit Kothari <Sumit.Kothari@amd.com>
    AMD-Internal: [CPUPL-5036]
    
    Change-Id: Ic68e55f51022578f774a6cc21c6abfb9c0b8ed4c

--- TESTING/LIN/psludriver.f
+++ TESTING/LIN/psludriver.f
@@ -470,7 +470,8 @@
 *                 If N < 0 in LU.dat file then PSGETRF API sets INFO = -2
                   IF ((M.LT.0 .AND. INFO.EQ.-1) .OR.
      $                (N.LT.0 .AND. INFO.EQ.-2)) THEN
-*                    If PSGETRF is returning correct error code we need to pass this case
+*                    If PSGETRF is returning correct error
+*                    code we need to pass this case
                      WRITE( NOUT, FMT = 9983 ) 'PSGETRF'
                      KPASS = KPASS + 1
                   ELSE
@@ -479,23 +480,13 @@
                   END IF
                   RCOND = ZERO
                   GO TO 30
-                  END IF
                ELSE IF (M.EQ.0 .OR. N.EQ.0) THEN
 *                 If M = 0 or N =0 this is the case of
 *                 early return from ScaLAPACK API.
 *                 If there is safe exit from API we need to pass this case
-                  IF(NOUT .LE. 0 .OR. NOUT .GT. 6) THEN
-*                 resetting stdout, corrupted by early return cases
-                    NOUT = 6
-                  END IF
                   WRITE( NOUT, FMT = 9982 ) 'PSGETRF'
                   KPASS = KPASS + 1
                   RCOND = ZERO
-                  IF(NAN_PERCENT .GT. 0 .OR.
-     $                INF_PERCENT .GT. 0) THEN
-*                    RESET EX-FLAG
-                     EX_FLAG = .TRUE.
-                  END IF
                   GO TO 30
                END IF
 *
@@ -1275,6 +1266,7 @@
      $        'Instead of driver file, we will handle this case from ',
      $        'ScaLAPACK API.')
  9983 FORMAT(  A, ' returned correct error code. Passing this case.')
+ 9982 FORMAT(  'This is safe exit from ', A, ' API. Passing this case.')
 *
       STOP
 *
