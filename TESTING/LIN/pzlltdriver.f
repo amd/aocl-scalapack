73a781390a80d8cd62254af9f78c8a7611b97b39
Sumit Kothari <skothari@amd.com>
2024-06-03 10:11:42 +0530

    Implemented functionality to validate incorrect inputs for the pzpotrf and pzpotrs APIs
    Signed-off-by: Sumit Kothari <Sumit.Kothari@amd.com>
    AMD-Internal: [CPUPL-5200]
    
    Change-Id: I9672fd3d58b8813e7d38224fb7ec19455d825f47

--- TESTING/LIN/pzlltdriver.f
+++ TESTING/LIN/pzlltdriver.f
@@ -326,20 +326,11 @@
                   GO TO 30
                END IF
 #else
-               IF(N .LT. 0 .AND. (IERR(1) .EQ. -2 .OR.
-     $              IERR(1) .EQ. -4 .OR. IERR(1) .EQ. -8 .OR.
-     $              IERR(1) .EQ. -3 .OR. IERR(1) .EQ. -12 )) THEN
-*                   DESCINIT returns the correct error code,
-*                   -2, -3 incase of invalid M and N
-*                   -4, -8 or -12 incase of incorrect grid info
-*                   MAIN API can be validated.
-*                   Do NOTHING
+*              If N < 0 in LLT.dat file then DESCINIT API sets IERR( 1 ) = -2
+               IF( N.LT.0 .AND. IERR( 1 ).EQ.-2 ) THEN
+*                 If DESCINIT is returning correct error code then
+*                 do nothing
                   WRITE( NOUT, FMT = 9984 ) 'N'
-*                   disable extreme value case when N < 0
-                    EX_FLAG = .FALSE.
-               ELSE IF(N .EQ. 0) THEN
-*                   disable extreme value case when M < 0
-                    EX_FLAG = .FALSE.
                ELSE IF( IERR( 1 ).LT.0 ) THEN
                   IF( IAM.EQ.0 )
      $               WRITE( NOUT, FMT = 9997 ) 'descriptor'
@@ -472,15 +463,14 @@
      $               WRITE( NOUT, FMT = * ) 'PZPOTRF INFO=', INFO
 *                 If N < 0 in LLT.dat file then PZPOTRF API sets INFO = -2
                   IF (N.LT.0 .AND. INFO.EQ.-2) THEN
-*                    If PZPOTRF is returning correct error code, do nothing
+*                    If PZPOTRF is returning correct error
+*                    code we need to pass this case
                      WRITE( NOUT, FMT = 9983 ) 'PZPOTRF'
-                  ELSE IF (INFO.GT.0 .AND. EX_FLAG)  THEN
-                     WRITE(*,*) 'PZPOTRF INFO=', INFO
-*                    do nothing, skip residual calculation
-*                    Pass this case in INF/NAN residual calculation
+                     KPASS = KPASS + 1
                   ELSE
 *                    For other error code we will mark test case as fail
                      KFAIL = KFAIL + 1
+                  END IF
                   RCOND = ZERO
                   GO TO 60
                   END IF
@@ -592,14 +582,9 @@
      $                              IERR( 1 ) )
 *                    If NRHS < 0 in LLT.dat file then
 *                    DESCINIT API sets IERR( 1 ) = -3
-                        IF (NRHS.LT.0 .AND. IERR( 1 ).EQ.-3 .OR.
-     $                           IERR(1) .EQ. -12) THEN
-*                             If DESCINIT is returns correct error code
+                     IF (NRHS.LT.0 .AND. IERR( 1 ).EQ.-3 ) THEN
+*                       If DESCINIT is returning correct error code then
 *                       do nothing
-                        IF(NOUT .LE. 0 .OR. NOUT .GT. 6) THEN
-*                       resetting stdout, corrupted by negative cases
-                           NOUT = 6
-                        END IF
                         WRITE( NOUT, FMT = 9984 ) 'NRHS'
                      END IF
 *
@@ -710,32 +695,23 @@
                      CALL SLTIMER( 2 )
 *
                      IF( INFO.NE.0 ) THEN
-                        IF(NOUT .LE. 0 .OR. NOUT .GT. 6) THEN
-*                       resetting stdout, corrupted by negative cases
-                           NOUT = 6
-                        END IF
                         IF( IAM.EQ.0 )
      $                     WRITE( NOUT, FMT = * ) 'PZPOTRS INFO=', INFO
 *                       If NRHS < 0 in LLT.dat file then
 *                       PZPOTRS API sets INFO = -3
-                        IF( NRHS.LT.0 .AND. INFO.EQ.-3 .OR.
-     $                          (N.LT.0 .AND. INFO.EQ.-2) )  THEN
+                        IF( NRHS.LT.0 .AND. INFO.EQ.-3 ) THEN
 *                          If PZPOTRS is returning correct error code then
 *                          we need to pass this case
                            WRITE( NOUT, FMT = 9983 ) 'PZPOTRS'
-                        ELSE IF( INFO .GT. 0 .AND. EX_FLAG) THEN
-                              WRITE(*,*) 'PZPOTRS INFO=', INFO
-*                             Do Nothing, Pass this case in residual calculation
+                           KPASS = KPASS + 1
                         ELSE
 *                          For other error code we will mark test case as fail
                            KFAIL = KFAIL + 1
-                           GO TO 60
                         END IF
+                        GO TO 60
                      END IF
-*
-                     IF( CHECK .AND. .NOT.(EX_FLAG) .AND.
-     $                      INFO .EQ. 0 .AND. N .GT. 0 .AND.
-     $                      NRHS .GT. 0) THEN
+
+                     IF( CHECK ) THEN
 *
 *                       check for memory overwrite
 *
@@ -1115,11 +1091,10 @@
  9987 FORMAT( 'END OF TESTS.' )
  9986 FORMAT( '||A - ', A4, '|| / (||A|| * N * eps) = ', G25.7 )
  9985 FORMAT( '||Ax-b||/(||x||*||A||*eps*N) ', F25.7 )
- 9984 FORMAT(  A4, ' < 0 case detected. ',
+ 9984 FORMAT(  A, ' < 0 case detected. ',
      $        'Instead of driver file, we will handle this case from ',
      $        'ScaLAPACK API.')
  9983 FORMAT(  A, ' returned correct error code. Passing this case.')
- 9982 FORMAT(  'This is safe exit from ', A, ' API. Passing this case.')
 *
       STOP
 *
