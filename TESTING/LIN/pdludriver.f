8b90969a7e3476bcaa9b07aa1b6ffdea40b0ea7e
sswathym <sswathym@amd.com>
2024-10-21 11:51:53 +0530

    pkgconfig update
    Updated the existing pkgconfig with lib path, description and
    versioning.
    
    Signed-off-by: Sundari H <Sundari.SwathyMeenaH@amd.com>
    AMD-Internal: [CPUPL-5909]
    
    Change-Id: I5027eceb65b4a00d6a23d684cf188d423d14c7e1

--- TESTING/LIN/pdludriver.f
+++ TESTING/LIN/pdludriver.f
@@ -3,7 +3,7 @@
 *  -- ScaLAPACK testing driver (version 1.7) --
 *     University of Tennessee, Knoxville, Oak Ridge National Laboratory,
 *     and University of California, Berkeley.
-*     Modifications Copyright (c) 2024 Advanced Micro Devices, Inc. All rights reserved.
+*     Modifications Copyright (c) 2024-2025 Advanced Micro Devices, Inc. All rights reserved.
 *
 *  Purpose
 *  ========
@@ -119,7 +119,7 @@
       DOUBLE PRECISION   CTIME( 2 ), WTIME( 2 )
       DOUBLE PRECISION, allocatable :: MEM (:)
 #endif
-      CHARACTER          SVERSION( 100 )
+      CHARACTER*100          SVERSION
 *     ..
 *     .. External Subroutines ..
       EXTERNAL           BLACS_BARRIER, BLACS_EXIT, BLACS_GET,
@@ -158,6 +158,9 @@
 #ifdef DYNAMIC_WORK_MEM_ALLOC
       allocate(MEM(MEMSIZ))
 #endif
+*
+*     Display AOCL-SCALAPACK Version
+*
       CALL BLACS_PINFO( IAM, NPROCS )
 
       IF (IAM.EQ.0) THEN
